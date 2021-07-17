bb9d74f0b2df6ab8e43162d9e5459e606b8631e1
Zach Puckett <zpuckettdesign@gmail.com>
2021-07-17 17:50:13 -0500

    seeds.js

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,14 +1,6 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Book {
-    bookId: String
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
   type User {
     _id: ID
     username: String
@@ -18,26 +10,41 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
-  input bookInput {
+  type Book {
     bookId: String
     authors: [String]
-    description: String
     title: String
+    description: String
     image: String
     link: String
   }
+
   type Query {
     me: User
+    users: [User]
+    user(username: String!): User
+    userById(_id: ID!): User
   }
-  type Auth {
-    token: ID
-    user: User
-  }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: bookInput): User
+    saveBook(input: BookInput!): User
     removeBook(bookId: String!): User
+  }
+
+  input BookInput {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -11,12 +11,11 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
-  // function for our authenticated routes
+
   authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
+
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
@@ -27,13 +26,12 @@ module.exports = {
     }
 
     try {
-      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
-    }
-    // return updated request object
+
     return req;
-  },
-};
+  }
+}
+}
