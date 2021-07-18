b8348f0e81574b815c497f78689e406159cdeec7
Zach Puckett <zpuckettdesign@gmail.com>
2021-07-17 19:30:00 -0500

    server typo fixed

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const path = require('path');
-const 
-
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
